86a1fe6e6867e8a07b40832b3d2f5dcd7518215c
0-9l23l7 <241458@students.au.edu.pk>
2025-12-14 17:58:38 +0500

    Create Binary Node.cpp

---
+++ TREES/Binary Node.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+template<typename T>
+class BinaryNode{
+  private:
+  T data;
+  BinaryNode<T>* left;
+  BinaryNode<T>* right;
+  public:
+  BinaryNode(T d){
+    data=d;
+    left=nullptr;
+    right=nullptr;
+  }
+  void setLeft(BinaryNode<T>* l){
+    left=l;}
+    void setRight(BinaryNode<T>* r){
+    right=r;}
+  T value() const{
+    return data;
+  }
+  BinaryNode<T>* RetrieveLeft(){
+    return left;
+  }
+  BinaryNode<T>* RetrieveRight(){
+    return right;
+  }
+  bool isLeaf() const {
+    return (left==nullptr)&&(right==nullptr);
+  }
+ 
+  int size(){
+    int L=(left!=nullptr)? RetrieveLeft()->size():0;
+    int R=(right!=nullptr)? RetrieveRight()->size():0;
+    return 1+L+R;
+    
+  }
+  int Height(){
+    //Leaf Height Starts From zero 
+    if(isLeaf()){return 0;}
+    int L=(left!=nullptr)? RetrieveLeft()->Height():0;
+    int R=(right!=nullptr)? RetrieveRight()->Height():0;
+    return 1+max(L,R);
+    
+  }
+  void clear(){
+    if(left!=nullptr)left->clear();
+    if(right!=nullptr)right->clear();
+   delete this;
+  }
+};
+int main(){
+  BinaryNode<string>* A=new BinaryNode<string>("A");
+  BinaryNode<string>* B=new BinaryNode<string>("B");
+  BinaryNode<string>* C=new BinaryNode<string>("C");
+  A->setLeft(B);
+  A->setRight(C);
+  cout<<A->size();
+  cout<<A->Height();
+  B->clear();
+  return 0;
+}
